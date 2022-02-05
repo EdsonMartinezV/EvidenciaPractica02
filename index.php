844d2bd36384884511087555a0834a60ec7d7485
EdsonMartinez <EdsonMaartinez@hotmail.com>
2022-02-05 13:29:44 -0600

    Implements Toy and RubiksCube in index

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edson Jahir Martinez Vasquez</title>
+</head>
+<body>
+    <?php
+        require 'vendor/autoload.php';
+        use EdsonMtz\PooEvidence\RubiksCube;
+        use EdsonMtz\PooEvidence\Toy;
 
-require 'vendor/autoload.php';
-use EdsonMtz\PooEvidence;
+        $toy1 = new Toy("Red ball", 10, "sports");
+        $cube1 = new RubiksCube("Moyu cube", 100, "puzzles", 3, "stickerless");
 
- echo "Hello world";
+        echo "<h1>".$toy1->getName()."</h1>";
+        echo "<p>".$toy1->describe()."</p>";
+        echo "<p>".$toy1->play()."</p>";
+
+        echo "<h1>".$cube1->getName()."</h1>";
+        echo "<p>".$cube1->describe()."</p>";
+        echo "<p>".$cube1->play()."</p>";
+        echo "<p>".$cube1->solve()."</p>";
+        echo "<p>".$cube1->solve(2)."</p>";
+    ?>
+</body>
+</html>
--- src/RubiksCube.php
+++ src/RubiksCube.php
@@ -9,15 +9,14 @@ class RubiksCube extends Toy{
     }
 
     public function describe():string{
-        parent::describe();
-        return "\nThis toy is also a rubik's cube with $this->layers layers and a $this->theme theme";
+        return parent::describe()."<br>This toy is also a rubik's cube with $this->layers layers and a $this->theme theme";
     }
 
     public function solve(int $nAttemp = null){
         if ($nAttemp == null) {
             echo "This toy is being solved";
         }else {
-            echo "This toy is being solved by the $nAttemp time";
+            echo "This toy is being solved by the ".$nAttemp."nd time";
         }
     }
 
